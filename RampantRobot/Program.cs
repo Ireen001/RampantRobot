412c0830cb2c9bfdf235420451c33a51e2450c5e
Ireen Heemels <ireen.heemels@hotmail.com>
2018-12-18 13:05:42 +0100

    Add project files.

---
+++ RampantRobot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RampantRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] grid;
+            const int row = 5;
+            const int col = 5;
+            string n;
+            grid = new string[row, col];
+
+
+
+            //fill the grid with '.' characters
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    grid[r, c] = ".";
+
+                }
+            }
+
+            grid[0, 0] = "M";
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    Console.Write(string.Format("{0} ", grid[r, c]));
+
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.WriteLine("Enter the steps:");
+            n = Console.ReadLine();
+            //Console.Write(string.Format("{0}", n.Length));
+            if (n == "s")
+            {
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RampantRobot/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RampantRobot
+{
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+            
+        }
+        public double GetArea()
+        {
+            return Width * Height;
+        }
+
+    }
+}
